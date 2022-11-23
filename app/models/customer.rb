de49783718660c53b32cc21f1d71c97f2579170c
Elson Otake <elsonotake@yahoo.com.br>
2022-11-23 13:00:33 -0300

    Copy code using new table name (customers)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories, dependent: :destroy
+  has_many :deals, dependent: :destroy, foreign_key: 'author_id'
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :deals
-  resources :categories
+  resources :categories do
+    resources :deals
+  end
   devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/categories', to: 'categories#index', as: 'customer'
+  get '/', to: 'splash#index'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'categories#index'
 end
